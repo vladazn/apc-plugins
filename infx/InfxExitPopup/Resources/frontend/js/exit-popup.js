6912b24d9b4b857b9bafac963e77aa46d1f5a6dd
vladazn <vladimir.aznauryan@gmail.com>
2018-03-19 00:46:25 +0400

    exitpopup

---
+++ infx/InfxExitPopup/Resources/frontend/js/exit-popup.js
@@ -0,0 +1,91 @@
+;(function ($,window,document) {
+
+    var exitPopup = {
+
+		cookie_name: 'infx_exit_popup_showed',
+		modalcontent: '.infx--modal',
+		modaltitle: '.infx--modal-title',
+
+		init: function() {
+			var me = this;
+
+			if(me.cookieExists()) {
+				return;
+			}
+
+			me.setCookie(me.cookie_name,'1',30);
+			me.trackCursor();
+		},
+
+		trackCursor: function() {
+			var me = this;
+			$(document).mousemove(function(e){
+				if(e.pageY < 5) {
+					me.showPopup();
+				}
+			});
+		},
+
+		showPopup: function() {
+			var me = this;
+
+			$.modal.open($(me.modalcontent).html(),{
+                title: $(me.modaltitle).text()
+            });
+			me.unbindTracking();
+		},
+
+		unbindTracking: function() {
+			var me = this;
+
+			$(document).off('mousemove');
+		},
+
+		setCookie: function(cname, cvalue, exdays) {
+			var me = this;
+
+			var d = new Date();
+
+			d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+
+			var expires = "expires=" + d.toUTCString();
+
+			document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+		},
+
+		getCookie: function(cname) {
+			var me = this;
+
+			var name = cname + "=";
+			var ca = document.cookie.split(';');
+			for(var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt(0) == ' ') {
+					c = c.substring(1);
+				}
+				if (c.indexOf(name) == 0) {
+					return c.substring(name.length, c.length);
+				}
+			}
+			return "";
+		},
+
+		cookieExists: function() {
+			var me = this;
+
+			var value = me.getCookie(me.cookie_name);
+
+			if (value == '1') {
+				return true;
+			}
+
+			return false;
+		},
+
+	};
+
+    $(document).ready(function(){
+       exitPopup.init();
+    });
+
+})($,window,document);
---
+++ infx/InfxExitPopup/Resources/views/frontend/index/index.tpl
@@ -0,0 +1,26 @@
+{extends file='parent:frontend/index/index.tpl'}
+
+{block name="frontend_index_footer" prepend}
+<div class="infx--modal is--hidden">
+    <div class="column--content">
+        <p class="column--desc">
+            {s namespace="frontend/infx/modal" name="NewsletterText"}<p>Wollen Sie wirklich schon gehen?</p><p>Melden Sie sich zum Newsletter an und bekommen Sie einen Rabattcode</p>{/s}
+        </p>
+
+		<form class="newsletter--form" action="{url controller='newsletter'}" method="post">
+			<input type="hidden" value="1" name="subscribeToNewsletter" />
+			<input type="hidden" value="1" name="infx_newsletter"/>
+
+			<input type="email" name="newsletter" class="newsletter--field" placeholder="{s namespace="frontend/index/menu_footer" name="IndexFooterNewsletterValue"}{/s}" />
+			{if {config name="newsletterCaptcha"} !== "nocaptcha"}
+				<input type="hidden" name="redirect">
+			{/if}
+
+			<button type="submit" class="newsletter--button btn">
+				<i class="icon--mail"></i> <span class="button--text">{s namespace="frontend/infx/modal" name='NewsletterSubmit'}Subscribe to newsletter{/s}</span>
+			</button>
+		</form>
+    </div>
+</div>
+<span class="infx--modal-title is--hidden">{s namespace="frontend/infx/modal" name='NewsletterTitle'}Newsletter{/s}</span>
+{/block}
